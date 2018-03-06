fc17c25ac09d8b758e538fac5a5c999ce52181a8
vasco <vastur@gmail.com>
2018-03-06 01:08:27 -0800

    Prevent background hits

--- src/object-color-tracker.js
+++ src/object-color-tracker.js
@@ -1,7 +1,9 @@
+const CS_BITS = 6; // How many bits to reserve for checksum. Will eat away into the usable size of the registry.
+
 const int2HexColor = num => `#${Math.min(num, Math.pow(2, 24)).toString(16).padStart(6, '0')}`;
 const rgb2Int = (r, g, b) => (r << 16) + (g << 8) + b;
 
-const CS_BITS = 6; // How many bits to reserve for checksum. Will eat away into the usable size of the registry.
+const checksum = n => n % Math.pow(2, CS_BITS);
 
 export default class {
   constructor() {
@@ -14,7 +16,7 @@ export default class {
     }
 
     const idx = this.registry.length;
-    const cs = idx % Math.pow(2, CS_BITS);
+    const cs = checksum(idx);
 
     const color = int2HexColor(idx + (cs << (24 - CS_BITS)));
 
@@ -25,10 +27,12 @@ export default class {
   lookup([r, g, b]) {
     const n = rgb2Int(r, g, b);
 
+    if (!n) return null; // 0 index is reserved for background
+
     const idx = n & (Math.pow(2, 24 - CS_BITS) - 1); // registry index
     const cs = (n >> (24 - CS_BITS)) & (Math.pow(2, CS_BITS) - 1); // extract bits reserved for checksum
 
-    if ((idx % Math.pow(2, CS_BITS)) !== cs || idx >= this.registry.length) return null; // failed checksum or registry out of bounds
+    if (checksum(idx) !== cs || idx >= this.registry.length) return null; // failed checksum or registry out of bounds
 
     return this.registry[idx];
   }
