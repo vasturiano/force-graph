c5dbbfa1e5b9e0e874c8b5df7302f6b59eb38dbc
vasco <vastur@gmail.com>
2020-03-04 18:24:41 -0800

    Add typescript declaration

---
+++ src/index.d.ts
@@ -0,0 +1,167 @@
+interface GraphData {
+  nodes: NodeObject[];
+  links: LinkObject[];
+}
+
+type NodeObject = object & {
+  id?: string | number;
+  x?: number;
+  y?: number;
+  vx?: number;
+  vy?: number;
+  fx?: number;
+  fy?: number;
+};
+
+type LinkObject = object & {
+  source?: string | number | NodeObject;
+  target?: string | number | NodeObject;
+};
+
+type Accessor<In, Out> = Out | string | ((obj: In) => Out);
+type NodeAccessor<T> = Accessor<NodeObject, T>;
+type LinkAccessor<T> = Accessor<LinkObject, T>;
+
+type CanvasCustomRenderFn<T> = (obj: T, canvasContext: CanvasRenderingContext2D, globalScale: number, isShadow?: boolean) => void;
+type CanvasCustomRenderMode = 'replace' | 'before' | 'after';
+
+type DagMode = 'td' | 'bu' | 'lr' | 'rl' | 'radialout' | 'radialin';
+
+interface ForceFn {
+  (alpha: number): void;
+  initialize?: (nodes: NodeObject[]) => void;
+}
+
+export interface ForceGraphGenericInstance<ChainableInstance> {
+  (element: HTMLElement): ChainableInstance;
+  resetProps(): ChainableInstance;
+  _destructor(): void;
+
+  // Data input
+  graphData(): GraphData;
+  graphData(data: GraphData): ChainableInstance;
+  nodeId(): string;
+  nodeId(id: string): ChainableInstance;
+  linkSource(): string;
+  linkSource(source: string): ChainableInstance;
+  linkTarget(): string;
+  linkTarget(target: string): ChainableInstance;
+
+  // Container layout
+  width(): number;
+  width(width: number): ChainableInstance;
+  height(): number;
+  height(height: number): ChainableInstance;
+  backgroundColor(): string;
+  backgroundColor(color?: string): ChainableInstance;
+
+  // Node styling
+  nodeRelSize(): number;
+  nodeRelSize(size: number): ChainableInstance;
+  nodeVal(): NodeAccessor<number>;
+  nodeVal(valAccessor: NodeAccessor<number>): ChainableInstance;
+  nodeLabel(): NodeAccessor<string>;
+  nodeLabel(labelAccessor: NodeAccessor<string>): ChainableInstance;
+  nodeColor(): NodeAccessor<string>;
+  nodeColor(colorAccessor: NodeAccessor<string>): ChainableInstance;
+  nodeAutoColorBy(): NodeAccessor<string>;
+  nodeAutoColorBy(colorByAccessor: NodeAccessor<string>): ChainableInstance;
+  nodeCanvasObject(): CanvasCustomRenderFn<NodeObject>;
+  nodeCanvasObject(renderFn: CanvasCustomRenderFn<NodeObject>): ChainableInstance;
+  nodeCanvasObjectMode(): string | ((obj: NodeObject) => CanvasCustomRenderMode);
+  nodeCanvasObjectMode(modeAccessor: string | ((obj: NodeObject) => CanvasCustomRenderMode)): ChainableInstance;
+
+  // Link styling
+  linkLabel(): LinkAccessor<string>;
+  linkLabel(labelAccessor: LinkAccessor<string>): ChainableInstance;
+  linkVisibility(): LinkAccessor<boolean>;
+  linkVisibility(visibilityAccessor: LinkAccessor<boolean>): ChainableInstance;
+  linkColor(): LinkAccessor<string>;
+  linkColor(colorAccessor: LinkAccessor<string>): ChainableInstance;
+  linkAutoColorBy(): LinkAccessor<string>;
+  linkAutoColorBy(colorByAccessor: LinkAccessor<string>): ChainableInstance;
+  linkWidth(): LinkAccessor<number>;
+  linkWidth(widthAccessor: LinkAccessor<number>): ChainableInstance;
+  linkCurvature(): LinkAccessor<number>;
+  linkCurvature(curvatureAccessor: LinkAccessor<number>): ChainableInstance;
+  linkCanvasObject(): CanvasCustomRenderFn<LinkObject>;
+  linkCanvasObject(renderFn: CanvasCustomRenderFn<LinkObject>): ChainableInstance;
+  linkCanvasObjectMode(): string | ((obj: LinkObject) => CanvasCustomRenderMode);
+  linkCanvasObjectMode(modeAccessor: string | ((obj: LinkObject) => CanvasCustomRenderMode)): ChainableInstance;
+  linkDirectionalArrowLength(): LinkAccessor<number>;
+  linkDirectionalArrowLength(lengthAccessor: LinkAccessor<number>): ChainableInstance;
+  linkDirectionalArrowColor(): LinkAccessor<string>;
+  linkDirectionalArrowColor(colorAccessor: LinkAccessor<string>): ChainableInstance;
+  linkDirectionalArrowRelPos(): LinkAccessor<number>;
+  linkDirectionalArrowRelPos(fractionAccessor: LinkAccessor<number>): ChainableInstance;
+  linkDirectionalParticles(): LinkAccessor<number>;
+  linkDirectionalParticles(numParticlesAccessor: LinkAccessor<number>): ChainableInstance;
+  linkDirectionalParticleSpeed(): LinkAccessor<number>;
+  linkDirectionalParticleSpeed(relDistancePerFrameAccessor: LinkAccessor<number>): ChainableInstance;
+  linkDirectionalParticleWidth(): LinkAccessor<number>;
+  linkDirectionalParticleWidth(widthAccessor: LinkAccessor<number>): ChainableInstance;
+  linkDirectionalParticleColor(): LinkAccessor<string>;
+  linkDirectionalParticleColor(colorAccessor: LinkAccessor<string>): ChainableInstance;
+  emitParticle(link: LinkObject): ChainableInstance;
+
+  // Render control
+  pauseAnimation(): ChainableInstance;
+  resumeAnimation(): ChainableInstance;
+  centerAt(): {x: number, y: number};
+  centerAt(x?: number, y?: number, durationMs?: number): ChainableInstance;
+  zoom(): number;
+  zoom(scale: number, durationMs?: number): ChainableInstance;
+
+  // Force engine (d3-force) configuration
+  dagMode(): DagMode;
+  dagMode(mode: DagMode): ChainableInstance;
+  dagLevelDistance(): number | null;
+  dagLevelDistance(distance: number): ChainableInstance;
+  d3AlphaDecay(): number;
+  d3AlphaDecay(alphaDecay: number): ChainableInstance;
+  d3VelocityDecay(): number;
+  d3VelocityDecay(velocityDecay: number): ChainableInstance;
+  d3Force(forceName: 'link' | 'charge' | 'center' | string): ForceFn | undefined;
+  d3Force(forceName: 'link' | 'charge' | 'center' | string, forceFn: ForceFn): ChainableInstance;
+  d3ReheatSimulation(): ChainableInstance;
+  warmupTicks(): number;
+  warmupTicks(ticks: number): ChainableInstance;
+  cooldownTicks(): number;
+  cooldownTicks(ticks: number): ChainableInstance;
+  cooldownTime(): number;
+  cooldownTime(milliseconds: number): ChainableInstance;
+  onEngineTick(callback: () => void): ChainableInstance;
+  onEngineStop(callback: () => void): ChainableInstance;
+
+  // Interaction
+  onNodeClick(callback: (node: NodeObject) => void): ChainableInstance;
+  onNodeRightClick(callback: (node: NodeObject) => void): ChainableInstance;
+  onNodeHover(callback: (node: NodeObject | null, previousNode: NodeObject | null) => void): ChainableInstance;
+  onNodeDrag(callback: (node: NodeObject, translate: { x: number, y: number }) => void): ChainableInstance;
+  onNodeDragEnd(callback: (node: NodeObject, translate: { x: number, y: number }) => void): ChainableInstance;
+  onLinkClick(callback: (link: LinkObject) => void): ChainableInstance;
+  onLinkRightClick(callback: (link: LinkObject) => void): ChainableInstance;
+  onLinkHover(callback: (link: LinkObject | null, previousLink: LinkObject | null) => void): ChainableInstance;
+  linkHoverPrecision(): number;
+  linkHoverPrecision(precision: number): ChainableInstance;
+  onBackgroundClick(callback: (event: MouseEvent) => void): ChainableInstance;
+  onBackgroundRightClick(callback: (event: MouseEvent) => void): ChainableInstance;
+  onZoom(callback: (transform: {k: number, x: number, y: number}) => void): ChainableInstance;
+  onZoomEnd(callback: (transform: {k: number, x: number, y: number}) => void): ChainableInstance;
+  enableNodeDrag(): boolean;
+  enableNodeDrag(enable: boolean): ChainableInstance;
+  enableZoomPanInteraction(): boolean;
+  enableZoomPanInteraction(enable: boolean): ChainableInstance;
+  enablePointerInteraction(): boolean;
+  enablePointerInteraction(enable?: boolean): ChainableInstance;
+
+  // Utility
+  screen2GraphCoords(x: number, y: number): { x: number, y: number };
+  graph2ScreenCoords(x: number, y: number): { x: number, y: number };
+}
+
+export type ForceGraphInstance = ForceGraphGenericInstance<ForceGraphInstance>;
+
+declare function ForceGraph(): ForceGraphInstance;
+
+export default ForceGraph;
